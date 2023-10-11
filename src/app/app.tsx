7312be6eb29c6873f6dd5f0abad3cece18e6ce98
Damian <ruiz.damian17@gmail.com>
2023-10-11 12:12:07 -0700

    Trivia Teams started

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,16 +1,34 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-
-import NxWelcome from './nx-welcome';
-
+import { useState } from 'react';
+import ClickToEditInput from './components/ClickToEditInput/ClickToEditInput';
 export function App() {
+  const [teamLeftName, setTeamLeftName] = useState<string>('Lefty-Lucy');
+  const [teamRightName, setTeamRightName] = useState<string>('Righty-Tighty');
   return (
-    <>
-      <NxWelcome title="soapstone-trivia" />
-
-      <div />
-    </>
+    <Trivia>
+      <Team>
+        <TeamName value={teamLeftName} setValue={setTeamLeftName} />
+      </Team>
+      <Team>
+        <TeamName value={teamRightName} setValue={setTeamRightName} />
+      </Team>
+    </Trivia>
   );
+}
+
+function Trivia({ children }: any) {
+  return children;
+}
+
+function Team({ children }: any) {
+  return children;
+}
+function TeamName({ value, setValue }: TeamNameProps) {
+  return <ClickToEditInput value={value} setValue={setValue} />;
+}
+
+interface TeamNameProps {
+  value: string;
+  setValue: (value: string) => void;
 }
 
 export default App;
